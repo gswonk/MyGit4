00a70d45c6c2d66a67f6718b9fc932cde579a07d
gswon <gswon@GSWONK-DEV>
2018-03-12 13:25:17 -0700

    30

--- t4/t4App.cs
+++ t4/t4App.cs
@@ -12,7 +12,7 @@ namespace t4
         {
             Console.WriteLine("Hello, world...");
 
-            for (var i=0; i<20; i++)
+            for (var i=0; i<30; i++)
             {
                 Console.WriteLine($"{i}.Hello, world...");
             }
